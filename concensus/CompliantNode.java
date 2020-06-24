3fc9c68d53b88824a971c9532b7d66f579a5e8e9
zyxjs <jason.sie@zyxentertainment.ca>
2020-06-23 20:55:44 -0700

    first submission

--- concensus/CompliantNode.java
+++ concensus/CompliantNode.java
@@ -1,26 +1,44 @@
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 /* CompliantNode refers to a node that follows the rules (not malicious)*/
 public class CompliantNode implements Node {
+    private double pFollow;
+    private double pMalicious;
+    private double pDistribution;
+    private double numRounds;
+    private Set<Transaction> transactions;
 
     public CompliantNode(double p_graph, double p_malicious, double p_txDistribution, int numRounds) {
         // IMPLEMENT THIS
+        pFollow = p_graph;
+        pMalicious = p_malicious;
+        pDistribution = p_txDistribution;
+        this.numRounds = numRounds;
+        transactions = new HashSet<>();
     }
 
     public void setFollowees(boolean[] followees) {
         // IMPLEMENT THIS
+        for (int i = 0; i < followees.length; ++i) {
+            followees[i] = Math.random() < pFollow;
+        }
     }
 
     public void setPendingTransaction(Set<Transaction> pendingTransactions) {
         // IMPLEMENT THIS
+        transactions = pendingTransactions;
     }
 
     public Set<Transaction> sendToFollowers() {
         // IMPLEMENT THIS
+        return transactions;
     }
 
     public void receiveFromFollowees(Set<Candidate> candidates) {
         // IMPLEMENT THIS
+        for (Candidate c : candidates) {
+            transactions.add(c.tx);
+        }
     }
 }
--- concensus/Simulation.java
+++ concensus/Simulation.java
@@ -14,34 +14,35 @@ public class Simulation {
    public static void main(String[] args) {
 
       // There are four required command line arguments: p_graph (.1, .2, .3),
-      // p_malicious (.15, .30, .45), p_txDistribution (.01, .05, .10), 
+      // p_malicious (.15, .30, .45), p_txDistribution (.01, .05, .10),
       // and numRounds (10, 20). You should try to test your CompliantNode
       // code for all 3x3x3x2 = 54 combinations.
 
       int numNodes = 100;
       double p_graph = Double.parseDouble(args[0]); // parameter for random graph: prob. that an edge will exist
       double p_malicious = Double.parseDouble(args[1]); // prob. that a node will be set to be malicious
-      double p_txDistribution = Double.parseDouble(args[2]); // probability of assigning an initial transaction to each node 
+      double p_txDistribution = Double.parseDouble(args[2]); // probability of assigning an initial transaction to each
+                                                             // node
       int numRounds = Integer.parseInt(args[3]); // number of simulation rounds your nodes will run for
 
       // pick which nodes are malicious and which are compliant
       Node[] nodes = new Node[numNodes];
       for (int i = 0; i < numNodes; i++) {
-         if(Math.random() < p_malicious)
+         if (Math.random() < p_malicious)
             // When you are ready to try testing with malicious nodes, replace the
             // instantiation below with an instantiation of a MaliciousNode
-            nodes[i] = new MalDoNothing(p_graph, p_malicious, p_txDistribution, numRounds);
+            nodes[i] = new MaliciousNode(p_graph, p_malicious, p_txDistribution, numRounds);
          else
             nodes[i] = new CompliantNode(p_graph, p_malicious, p_txDistribution, numRounds);
       }
-
 
       // initialize random follow graph
       boolean[][] followees = new boolean[numNodes][numNodes]; // followees[i][j] is true iff i follows j
       for (int i = 0; i < numNodes; i++) {
          for (int j = 0; j < numNodes; j++) {
-            if (i == j) continue;
-            if(Math.random() < p_graph) { // p_graph is .1, .2, or .3
+            if (i == j)
+               continue;
+            if (Math.random() < p_graph) { // p_graph is .1, .2, or .3
                followees[i][j] = true;
             }
          }
@@ -60,26 +61,26 @@ public class Simulation {
          validTxIds.add(r);
       }
 
-
       // distribute the 500 Transactions throughout the nodes, to initialize
       // the starting state of Transactions each node has heard. The distribution
       // is random with probability p_txDistribution for each Transaction-Node pair.
       for (int i = 0; i < numNodes; i++) {
          HashSet<Transaction> pendingTransactions = new HashSet<Transaction>();
-         for(Integer txID : validTxIds) {
+         for (Integer txID : validTxIds) {
             if (Math.random() < p_txDistribution) // p_txDistribution is .01, .05, or .10.
                pendingTransactions.add(new Transaction(txID));
          }
          nodes[i].setPendingTransaction(pendingTransactions);
       }
 
-
       // Simulate for numRounds times
       for (int round = 0; round < numRounds; round++) { // numRounds is either 10 or 20
 
-         // gather all the proposals into a map. The key is the index of the node receiving
+         // gather all the proposals into a map. The key is the index of the node
+         // receiving
          // proposals. The value is an ArrayList containing 1x2 Integer arrays. The first
-         // element of each array is the id of the transaction being proposed and the second
+         // element of each array is the id of the transaction being proposed and the
+         // second
          // element is the index # of the node proposing the transaction.
          HashMap<Integer, Set<Candidate>> allProposals = new HashMap<>();
 
@@ -90,13 +91,14 @@ public class Simulation {
                   continue; // ensure that each tx is actually valid
 
                for (int j = 0; j < numNodes; j++) {
-                  if(!followees[j][i]) continue; // tx only matters if j follows i
+                  if (!followees[j][i])
+                     continue; // tx only matters if j follows i
 
                   if (!allProposals.containsKey(j)) {
-                	  Set<Candidate> candidates = new HashSet<>();
-                	  allProposals.put(j, candidates);
+                     Set<Candidate> candidates = new HashSet<>();
+                     allProposals.put(j, candidates);
                   }
-                  
+
                   Candidate candidate = new Candidate(tx, i);
                   allProposals.get(j).add(candidate);
                }
@@ -123,6 +125,4 @@ public class Simulation {
 
    }
 
-
 }
-
